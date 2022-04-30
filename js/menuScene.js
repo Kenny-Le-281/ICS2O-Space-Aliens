b13f6fca98092e38dab388b1cfe6017d7c8f94ad
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-30 15:17:39 +0000

    added menuScene.js

---
+++ js/menuScene.js
@@ -0,0 +1,49 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Kenny Le All rights reserved
+//
+// Created by: Kenny Le
+// Created on: Apr 2022
+// This is the Menu Scene
+
+/**
+ * This class is the Menu Scene.
+ */
+class MenuScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "menuScene" })
+  }
+
+  /**
+   * This method is the init.
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * This method is the preload.
+   */
+  preload() {
+    console.log("Menu Scene")
+  }
+
+  /**
+   * This method is the create.
+   */
+  create(data) {
+    // pass
+  }
+
+  /**
+   * This method is the update.
+   */
+  update(time, delta) {
+    // pass
+  }
+}
+
+export default MenuScene
