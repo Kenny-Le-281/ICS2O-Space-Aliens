49273f3eb8eda0362eb35505638b2100dc258a57
Kenny-Le-281 <Kenny-Le-281@users.noreply.github.com>
2022-05-03 16:44:47 +0000

    Prettified Code!

--- js/menuScene.js
+++ js/menuScene.js
@@ -32,35 +32,30 @@ class MenuScene extends Phaser.Scene {
    */
   preload() {
     console.log("Menu Scene")
-    this.load.image('menuSceneBackground', 'assets/aliens_screen_image2.jpg')
-    this.load.image('startButton', 'assets/start.png')
+    this.load.image("menuSceneBackground", "assets/aliens_screen_image2.jpg")
+    this.load.image("startButton", "assets/start.png")
   }
 
   /**
    * This method is the create.
    */
   create(data) {
-    this.menuSceneBackgroundImage = this.add.sprite(
-      0,
-      0,
-      "menuSceneBackground"
-    )
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
-    this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton")
     this.startButton.setInteractive({ useHandCursor: true })
-    this.startButton.on('pointerdown', () => this.clickButton())
+    this.startButton.on("pointerdown", () => this.clickButton())
     // pass
   }
 
   /**
    * This method is the update.
    */
-  update(time, delta) {
-  }
-    clickButton () {
-      this.scene.start('gameScene')
+  update(time, delta) {}
+  clickButton() {
+    this.scene.start("gameScene")
     // pass
   }
 }
