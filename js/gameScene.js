9ee1ab7989dbc992bdc9624fa3d91b549bc2a8f2
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-13 12:45:50 -0400

    changes in gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,7 +9,7 @@
 /**
  * This class is the Menu Scene.
  */
-class GameScene extends Phaser.Scene {
+ class GameScene extends Phaser.Scene {
   /**
    * This method is the construtor.
    */
@@ -38,6 +38,8 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
     this.load.image("missile", "assets/missile.png")
+    // sound
+    this.load.audio('laser', 'assets/laser1.wav')
   }
 
   /**
@@ -82,18 +84,22 @@ class GameScene extends Phaser.Scene {
       if (this.fireMissile === false) {
         // fire missile
         this.fireMissile = true
-        const aNewMissile = this.physics.add.sprite(
-          this.ship.x,
-          this.ship.y,
-          "missile"
-        )
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
         this.missileGroup.add(aNewMissile)
+        this.sound.play('laser')
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
     // pass
   }
 }
