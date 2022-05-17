864ef2b1a12e32a93cffb06318055648fcf8f6e6
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-17 12:54:36 -0400

    changes in gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,11 +10,10 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
+
   /**
    * This method creates an Alien.
    */
-
-  // create an alien
   createAlien() {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50
@@ -23,13 +22,11 @@ class GameScene extends Phaser.Scene {
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
-    // pass
   }
 
   /**
    * This method is the construtor.
    */
-  // constructor
   constructor() {
     super({ key: "gameScene" })
 
