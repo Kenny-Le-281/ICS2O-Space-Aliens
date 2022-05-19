5af09f360d1b2c125dc3daa3e1a864e68e66b17d
Kenny-Le-281 <Kenny-Le-281@users.noreply.github.com>
2022-05-19 02:30:12 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -74,13 +74,17 @@ class GameScene extends Phaser.Scene {
     this.createAlien()
 
     // Collisions between missiles and aliens
-    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide){
-      alienCollide.destroy()
-      missileCollide.destroy()
-      this.sound.play('explosion')
-      this.createAlien()
-      this.createAlien()
-    }.bind(this))
+    this.physics.add.collider(
+      this.missileGroup,
+      this.alienGroup,
+      function (missileCollide, alienCollide) {
+        alienCollide.destroy()
+        missileCollide.destroy()
+        this.sound.play("explosion")
+        this.createAlien()
+        this.createAlien()
+      }.bind(this)
+    )
   }
 
   /**
