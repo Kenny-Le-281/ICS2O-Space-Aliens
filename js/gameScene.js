1e9d37bbacbff25809481c084b2335150caaaf86
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-26 17:28:30 +0000

    changes in gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -37,6 +37,9 @@ class GameScene extends Phaser.Scene {
     this.score = 0
     this.scoreText = null
     this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
+    
+    this.gameOverText = null
+    this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
   }
 
   /**
@@ -60,6 +63,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "assets/laser1.wav")
     this.load.audio("explosion", "assets/barrelExploding.wav")
+    this.load.audio("bomb", "assets/bomb.wav")
   }
 
   /**
@@ -80,8 +84,8 @@ class GameScene extends Phaser.Scene {
     this.alienGroup = this.add.group()
     this.createAlien()
 
-    // Collisions between missiles and aliens
-    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+  // Collisions between missiles and aliens
+  this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
     alienCollide.destroy()
     missileCollide.destroy()
     this.sound.play('explosion')
@@ -90,7 +94,19 @@ class GameScene extends Phaser.Scene {
     this.createAlien()
     this.createAlien()
   }.bind(this))
+
+  // Collisions between ship and aliens
+  this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+    this.sound.play('bomb')
+    this.physics.pause()
+    alienCollide.destroy()
+    shipCollide.destory()
+    this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over! \nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+    this.gameOverText.setInteractive({ useHandCursor: true })
+    this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+  }.bind(this))
 }
+  
   /**
    * This method is the update.
    */
