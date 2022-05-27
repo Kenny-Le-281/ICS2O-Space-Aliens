66aa98e78a71b1e88862fc929cf8fa4b247ea3cd
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-27 16:15:13 +0000

    fixed gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -100,7 +100,7 @@ class GameScene extends Phaser.Scene {
     this.sound.play('bomb')
     this.physics.pause()
     alienCollide.destroy()
-    shipCollide.destory()
+    shipCollide.destroy()
     this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over! \nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
     this.gameOverText.setInteractive({ useHandCursor: true })
     this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
